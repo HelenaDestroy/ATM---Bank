bbf2e81fef300e6d5a1449448394f6bc01f72c7e
jess <jesicatorti@gmail.com>
2022-02-17 06:15:06 -0300

    first of day

--- script.js
+++ script.js
@@ -1,60 +1,48 @@
-class Billete {
+class Billete 
+{
   constructor(v, c){
-    this.valor = v,
-    this.cantidad= c
+    this.valor = v;
+    this.cantidad= c;
   }
 }
 
 function entregarDinero()
 {
-for(bi of caja){  
-console.log(bi);
-} //ir probando lo que haces
-}
+    for(var bi of caja)
+    {  
+      if (dinero > 0)         //NO PONER EL IF NOT, PORQUE YA SE SALE DEL CICLO Y PUNTO.
+        { div = Math.floor(dinero / bi.valor);
+          console.log(div);
+        }
+    }
+}  
+//SE PASA POR MATHFLOOR TODO. atributo valor de clase b. por eso b.valor
+  // if (div < b.cantidad){
+  //   papeles = div;}
+  // if (div > b.cantidad)
+  // {
+  //   papeles = b.cantidad;}
+  // }
+  // e.push (new Billete (b.valor, papeles))
+  // dinero = dinero - (b.valor * papeles)
+  // }
+  
+
+ //ir probando lo que haces
+
+
 var caja=[];
+var entregado = [];  //variable de array que se comporta igual al de caja pero estos son los que entrego al usuario, no los que tengo en caja, se modificará a medida que modifique el algoritmo.
 caja.push (new Billete(50, 3));  //AGREGAMOS LOS BILLETES, DEBERIAMOS PODER METERLE CUALQUEIR BILLETE.
 caja.push (new Billete(20, 2));
 caja.push (new Billete(10, 3));
-
 var dinero = 210; //esto vendra del input user luego
-var entregado = []  //variable de array que se comporta igual al de caja pero estos son los que entrego al usuario, no los que tengo en caja, se modificará a medida que modifique el algoritmo.
 var div = 0;// inicializamos en 0
 var papeles = 0; // empieza en cero
-
 //una vez el usuario elige la cantidad de dinero, cuando pone extraer comienza todo.  necesito id boton
 var b = document.getElementById("extraer");
 b.addEventListener("click", entregarDinero);
 
-
-
-// dinero = 
-// entregado = 
-// papeles =
-// div =
-// b.valor
-// b.cantidad
-
-// for (b of caja){
-// if (dinero < 0){ 
-//   return false  //ver que carajo retornaa
-// }
-// if (dinero < 0){
-//   div = Math.floor(dinero / b.valor)
-// if (div < b.cantidad){
-//   papeles = div;}
-// if (div > b.cantidad)
-// {
-//   papeles = b.cantidad;}
-// }
-// e.push (new Billete (b.valor, papeles))
-// dinero = dinero - (b.valor * papeles)
-// }
-
-
-
-// var billete50 = 50;
-// var billete20 = 20;
-// var billete10 = 10;
 
 // billetes50_quetengo = 3;
 
