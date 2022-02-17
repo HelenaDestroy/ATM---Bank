898c9097617ae1b3957d38545e6c503ddb666cae
jess <jesicatorti@gmail.com>
2022-02-17 12:29:52 -0300

    terminado lo principalE

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
 class Billete 
 {
-  constructor(v, c){
+  constructor(v, c)
+  {
     this.valor = v;
-    this.cantidad= c;
+    this.cantidad = c;
   }
 }
 
 function entregarDinero()
-{
-    for(var bi of caja)
-    {  
-      if (dinero > 0)         //NO PONER EL IF NOT, PORQUE YA SE SALE DEL CICLO Y PUNTO.
-        { div = Math.floor(dinero / bi.valor);
-          console.log(div);
+{ 
+  var t = document.getElementById("usuario");
+  dinero = parseInt(t.value);
+          for(var bi of caja)  
+        {  
+          if (dinero > 0)         
+          { 
+            div = Math.floor(dinero / bi.valor);
+            if   (div > bi.cantidad)
+              {
+                papeles = bi.cantidad;
+              }
+             else 
+              {
+                papeles = div; 
+              }  
+              entregado.push (new Billete (bi.valor, papeles) );  
+              dinero = dinero - (bi.valor * papeles);
+          }
+        }     
+      document.write("Usted retiró:");  
+      if (dinero > 0)  
+        {
+          for (var e of entregado)
+          {
+         
+          document.write("<br/>" + e.cantidad + " Billetes de $" + e.valor + ".<br/>")
+        
         }
-    }
-}  
-//SE PASA POR MATHFLOOR TODO. atributo valor de clase b. por eso b.valor
-  // if (div < b.cantidad){
-  //   papeles = div;}
-  // if (div > b.cantidad)
-  // {
-  //   papeles = b.cantidad;}
-  // }
-  // e.push (new Billete (b.valor, papeles))
-  // dinero = dinero - (b.valor * papeles)
-  // }
+          document.write("Bien... Usted pidió " + parseInt(t.value) + " pesos. Pero tiene que considerar que le cobramos intereses... por lo tanto los " + dinero + " pesos que faltan nos los quedamos nosotros pa' la birra. Gracias!" ); 
+          
+        }
+    else 
+      {  
+        for (var e of entregado)
+          {
+            document.write ("<hr/>" + e.cantidad + " billetes de $" + e.valor + "<br/>"); 
+          } 
+          
+      }
   
-
- //ir probando lo que haces
-
-
+}
 var caja=[];
-var entregado = [];  //variable de array que se comporta igual al de caja pero estos son los que entrego al usuario, no los que tengo en caja, se modificará a medida que modifique el algoritmo.
-caja.push (new Billete(50, 3));  //AGREGAMOS LOS BILLETES, DEBERIAMOS PODER METERLE CUALQUEIR BILLETE.
-caja.push (new Billete(20, 2));
-caja.push (new Billete(10, 3));
-var dinero = 210; //esto vendra del input user luego
-var div = 0;// inicializamos en 0
-var papeles = 0; // empieza en cero
-//una vez el usuario elige la cantidad de dinero, cuando pone extraer comienza todo.  necesito id boton
+var entregado = [];  
+caja.push (new Billete (50, 20) );
+caja.push (new Billete (20, 30) );
+caja.push (new Billete (10, 10) );
+
+var dinero = 0;  
+var div = 0;
+var papeles = 0;
+
 var b = document.getElementById("extraer");
 b.addEventListener("click", entregarDinero);
 
 
-// billetes50_quetengo = 3;
 
 
-// prompt("INGRESE EL VALOR A EXTRAER");
-// var request_user = prompt.value;
 
-// function cajero(){
-// cuantos_de_50 = (request_user / billete50);
-// //2.xxxx      =       //  110 / 50
-// listo50 = request_user - (Math.floor(cuantos50) * billete50);
-//   10 =            110    - (2 *                       50)
-// listo20 = (listo50 / billete20);
-// 0.5           =  10 / 20
-// cuantos_de_10 = (listo20 / billete 10)
-// request_user - (Math.floor(cuantos50) * billete10);
+
+
