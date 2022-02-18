1863d48abfdac02693623d95a69a11a9fb7d92c2
jess <jesicatorti@gmail.com>
2022-02-18 00:43:25 -0300

    improve text

--- script.js
+++ script.js
@@ -28,34 +28,31 @@ function entregarDinero()
               dinero = dinero - (bi.valor * papeles);
           }
         }     
-        resultado.innerHTML = "Usted retiró:";  
-      if (dinero > 0)  
-        {
-          for (var e of entregado)
+if (dinero > 0)  
+    {resultado.innerHTML = resultado.innerHTML + "Usted retiró:<br/>" 
+      for (var e of entregado)
+        { if (e.cantidad > 0)
           {
-          resultado.innerHTML = "<br/>" + e.cantidad + " Billetes de $" + e.valor + ".<br/>";
-         
+          resultado.innerHTML = resultado.innerHTML + e.cantidad + " billetes de $" + e.valor + ".</br>";
+          }
+               
         }
-        
-        resultado.innerHTML = "<hr/>Bien... Usted pidió " + parseInt(t.value) + " pesos. Pero tiene que considerar que le cobramos intereses... <br/>Por lo tanto los " + dinero + " pesos que faltan nos los quedamos nosotros pa' la birra. Gracias!"; 
-                
-        }
+        resultado.innerHTML = resultado.innerHTML + "<hr/>Sabemos que usted pidió " + parseInt(t.value) + " pesos. Pero tiene que considerar que le cobramos intereses... <br/>Por lo tanto los " + dinero + " pesos que faltan nos los quedamos nosotros pa' la birra. Gracias!<br/><br/>  " 
+    }
     else 
       {  
         for (var e of entregado)
-          {
-            resultado.innerHTML("<hr/>" + e.cantidad + " billetes de $" + e.valor + "<br/>"); 
-          } 
-          
+        {
+          resultado.innerHTML = resultado.innerHTML + "Usted retiró: " +  e.cantidad + " billetes de $" + e.valor + ".<br/>"; 
+        } 
       }
-  
 }
+
 var caja=[];
 var entregado = [];  
 caja.push (new Billete (50, 20) );
 caja.push (new Billete (20, 30) );
 caja.push (new Billete (10, 10) );
-
 var dinero = 0;  
 var div = 0;
 var papeles = 0;
@@ -63,9 +60,3 @@ var papeles = 0;
 var b = document.getElementById("extraer");
 b.addEventListener("click", entregarDinero);
 resultado = document.getElementById("parrafojs");
-
-
-
-
-
-
